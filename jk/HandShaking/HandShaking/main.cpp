accff5f0adcb61023a1cc6c35c1900c1b2554b96
JunKyo <snk114@daum.net>
2016-08-31 21:35:08 +0900

    HandShaking bug fix

--- jk/HandShaking/HandShaking/main.cpp
+++ jk/HandShaking/HandShaking/main.cpp
@@ -8,7 +8,13 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
+template<typename T1, typename T2>
+void TestEqual(const T1 && expected, const T2 && returns)
+{
+	if (expected == returns) std::cout << "Passed" << std::endl;
+	else std::cout << "Failed: " << returns << ", not: " << expected << std::endl;
+}
 
 class HandShaking
 {
@@ -19,37 +25,32 @@ public:
 	long countPerfect(int n) {
 		if (n % 2 != 0) return 0;
 		
-		auto hs = std::vector<long>(n + 1, 0);
-		hs[1] = hs[2] = 1;
+		const auto MAX_LOOP = n / 2;
+		auto hs = std::vector<long>(MAX_LOOP + 1, 0);
+		hs[0] = hs[1] = 1;
 		
-		for (auto _1 = 4; _1 <= n; _1 += 2) {
-			const auto base = long{_1 / 2};
-			const auto a = 3 < base ? base % 2 : 0;
-			const auto LOOP = _1 / 2 + a;
-			
-			for (auto _2 = 0; _2 < LOOP; ++_2) {
-				
-				hs[_1] += hs[_1 - LOOP + _2 - 1];
+		for (auto _1 = 2; _1 <= MAX_LOOP; ++_1) {
+			for (auto _2 = 0; _2 < _1; ++_2) {
+				hs[_1] += hs[_2] * hs[_1 - _2 - 1];
 			}
-			hs[_1 - 1] = hs[_1];
 		}
 		
-		return hs[n];
+		return hs[n / 2];
 	};
-
+	
 private:
 	HandShaking(const HandShaking &) = delete;
 	HandShaking& operator=(const HandShaking &) = delete;
 };
 
 int main(int argc, const char * argv[]) {
-	HandShaking obj;
-	std::cout << obj.countPerfect(2) << "\n";
-	std::cout << obj.countPerfect(4) << "\n";
-	std::cout << obj.countPerfect(6) << "\n";
-	std::cout << obj.countPerfect(8) << "\n";
-	std::cout << obj.countPerfect(10) << "\n";
-	std::cout << obj.countPerfect(12) << "\n";
+	auto obj = std::make_shared<HandShaking>();
+	TestEqual(obj->countPerfect(2), 1);
+	TestEqual(obj->countPerfect(4), 2);
+	TestEqual(obj->countPerfect(6), 5);
+	TestEqual(obj->countPerfect(8), 14);
+	TestEqual(obj->countPerfect(10), 42);
+	TestEqual(obj->countPerfect(16), 1430);
 	
     return 0;
 }
